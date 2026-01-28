2aabed6dd923146ba439195945519877841251d8
Seongjin Kang <65354945+enKODING1@users.noreply.github.com>
2026-01-28 15:10:22 +0900

    feat: php-fpm구성과 php프록싱 활성화

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 echo "hello world"
 
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+mv wp-cli.phar /usr/local/bin/wp
+wp --info
+
+cd /var/www/html
+chmod -R 755 /var/www/html
+rm -rf *
+
+wp core download --allow-root
+
+sed -i 's/listen = \/run\/php\/php.*\.sock/listen = 9000/g' /etc/php/*/fpm/pool.d/www.conf
+mkdir -p /run/php
+/usr/sbin/php-fpm* -F
